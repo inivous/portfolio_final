268a6dccc5c205e2e27a73fe45936f499b91b1bc
Ethan Resnick <ethan@trillium.host>
2024-01-25 12:17:03 -0500

    Update CONFIG.ts

--- CONFIG.ts
+++ CONFIG.ts
@@ -30,8 +30,8 @@ const CONFIG = {
 	links: [
 		{ enabled: false, name: "Facebook", url: "https://www.facebook.com/your_facebook", order: 1 },
 		{ enabled: false, name: "Twitter", url: "https://twitter.com/your_twitter", order: 2 },
-		{ enabled: false, name: "Instagram", url: "https://www.instagram.com/your_instagram", order: 3 },
-		{ enabled: true, name: "GitHub", url: "https://github.com/inivous", order: 4 },
+		{ enabled: true, name: "Instagram", url: "https://www.instagram.com/trillium.ethan", order: 4 },
+		{ enabled: true, name: "GitHub", url: "https://github.com/inivous", order: 5 },
 		{ enabled: false, name: "LinkedIn", url: "https://www.linkedin.com/in/your_linkedin", order: 5 },
 		{ enabled: false, name: "Pinterest", url: "https://www.pinterest.com/your_pinterest", order: 6 },
 		{ enabled: false, name: "Snapchat", url: "https://www.snapchat.com/add/your_snapchat", order: 7 },
@@ -41,7 +41,7 @@ const CONFIG = {
 		{ enabled: false, name: "YouTube", url: "https://www.youtube.com/channel/your_youtube", order: 11 },
 		{ enabled: false, name: "Twitch", url: "https://www.twitch.tv/your_twitch", order: 12 },
 		{ enabled: false, name: "Reddit", url: "https://www.reddit.com/user/your_reddit", order: 13 },
-		{ enabled: false, name: "Spotify", url: "https://open.spotify.com/user/your_spotify", order: 14 },
+		{ enabled: true, name: "Spotify", url: "https://open.spotify.com/user/31u6gw5nvyrrhkbxh5spopnlxpcu", order: 6 },
 		{ enabled: false, name: "SoundCloud", url: "https://soundcloud.com/your_soundcloud", order: 15 },
 		{ enabled: false, name: "Steam", url: "https://steamcommunity.com/id/your_steam", order: 16 },
 		{ enabled: true, name: "Discord", url: "https://discord.com/1088609130392068207", order: 3 },
@@ -49,7 +49,7 @@ const CONFIG = {
 		{ enabled: true, name: "Website", url: "https://trillium.host", order: 1 },
 	],
 	// Optional footer text. Leave empty "" to disable.
-	footer: "I'm not too active with social media; more may com soon.",
+	footer: "I'm not too active with social media; more may come soon.",
 };
 
 export default CONFIG;
